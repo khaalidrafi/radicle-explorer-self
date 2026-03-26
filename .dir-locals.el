0c8c878021742b130fcdd11c0233f80ccd438344
Khalid Rafi <khaalidrafi@gmail.com>
2026-03-26 22:17:40 +0600

    Refactor Emacs setup with new macro

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,17 +1,7 @@
 ((nil . (
          (eval .
-               (defun radicle-explorer/npm-server ()
-                 (interactive)
-                 (khalid/project-run-async-shell-command "npm start" "*radicle-explorer-self-server*")))
-
-         (eval .
-               (defun radicle-explorer/deploy ()
-                 (interactive)
-                 (khalid/project-run-async-shell-command "npm run deploy" "*radicle-explorer-self-deploy*")))
-         
-         (khalid/project-commands . (radicle-explorer/npm-server))
-
-         (khalid/project-deploy-command . radicle-explorer/deploy)
+               (project-make-commands
+                "server" "npm start"
+                "deploy" "npm run deploy"))
          (khalid/project-local-url . "http://localhost:3000")
-         (eval .
-               (khalid/set-project-command-keybindings)))))
+         )))
