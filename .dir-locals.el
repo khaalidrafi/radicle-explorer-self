5cb702666593d934da131ebe4098992e36d028f5
Khalid Rafi <khaalidrafi@gmail.com>
2026-03-27 10:23:24 +0600

    Add more Emacs commands

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,10 @@
-((nil . (
+((nil . ((eval . (prettier-js-mode))
          (eval .
                (project-make-commands
-                "server" "npm start"
+                "repos" "npm run repos"
+                "worker" "npm run worker"
+                "reload" "npm run reload"
+                "server" "npm run dev"
                 "deploy" "npm run deploy"))
          (khalid/project-local-url . "http://localhost:3000")
          )))
